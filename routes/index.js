af6ee58441f726975af95595f6773f29cbca8883
RAFAELMH809 <melchorhernandezrafael057@gmail.com>
2025-04-17 10:49:37 -0600

    intento1

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const controllers = require('../controllers');
 
 const router = Router();
 
-router.get('/', (req, res) => res.send('Welcome'))
-router.post('/users', controllers.createUser)
-router.get('/users', controllers.getAllUsers)
+router.get('/',  (req, res) => res.send('Welcome'));
+
+router.post('/users', controllers.createUser);
+router.get('/users', controllers.getAllUsers);
+router.delete('/users/:id', controllers.deleteUser);
+router.put('/users/:id', controllers.updateUser);
 
 module.exports = router;
